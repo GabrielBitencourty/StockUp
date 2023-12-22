4717908de7ca22907f316d815dde8dcb17355b37
Gabriel Bitencourt <gb750779@gmail.com>
2023-12-22 09:57:17 -0300

    HomePage

--- src/pages/UserPage.vue
+++ src/pages/UserPage.vue
@@ -1,8 +1,362 @@
 <template>
-<q-page class="flex flex-center">
-    <div>
-      <h4>Olá{{ user.user_metadata.name }}, seja bem vindo.</h4>
-    </div>
+<q-page>
+
+  <div id="homeCard" class="q-gutter-xl flex">
+
+    <q-card class="my-card" flat bordered>
+      <q-img src="https://cdn.quasar.dev/img/chicken-salad.jpg" />
+
+      <q-card-section>
+        <q-btn
+          fab
+          color="primary"
+          icon="place"
+          class="absolute"
+          style="top: 0; right: 12px; transform: translateY(-50%);"
+        />
+
+        <div class="row no-wrap items-center">
+          <div class="col text-h6 ellipsis">
+            Cafe Basilico
+          </div>
+          <div class="col-auto text-grey text-caption q-pt-md row no-wrap items-center">
+            <q-icon name="place" />
+            250 ft
+          </div>
+        </div>
+
+        <q-rating v-model="stars" :max="5" size="32px" />
+      </q-card-section>
+
+      <q-card-section class="q-pt-none">
+        <div class="text-subtitle1">
+          $・Italian, Cafe
+        </div>
+        <div class="text-caption text-grey">
+          Small plates, salads & sandwiches in an intimate setting.
+        </div>
+      </q-card-section>
+
+      <q-separator />
+
+      <q-card-actions>
+        <q-btn flat round icon="event" />
+        <q-btn flat color="primary">
+          Reserve
+        </q-btn>
+      </q-card-actions>
+    </q-card>
+
+    <q-card class="my-card" flat bordered>
+      <q-img src="https://images.pexels.com/photos/1639557/pexels-photo-1639557.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
+
+      <q-card-section>
+        <q-btn
+          fab
+          color="primary"
+          icon="place"
+          class="absolute"
+          style="top: 0; right: 12px; transform: translateY(-50%);"
+        />
+
+        <div class="row no-wrap items-center">
+          <div class="col text-h6 ellipsis">
+            Hamburguer
+          </div>
+          <div class="col-auto text-grey text-caption q-pt-md row no-wrap items-center">
+            <q-icon name="place" />
+            250 ft
+          </div>
+        </div>
+
+        <q-rating v-model="stars" :max="5" size="32px" />
+      </q-card-section>
+
+      <q-card-section class="q-pt-none">
+        <div class="text-subtitle1">
+          $・Brazilian, Burguer
+        </div>
+        <div class="text-caption text-grey">
+          Small plates, salads & sandwiches in an intimate setting.
+        </div>
+      </q-card-section>
+
+      <q-separator />
+
+      <q-card-actions>
+        <q-btn flat round icon="event" />
+        <q-btn flat color="primary">
+          Reserve
+        </q-btn>
+      </q-card-actions>
+    </q-card>
+
+    <q-card class="my-card" flat bordered>
+      <q-img src="https://images.pexels.com/photos/3915915/pexels-photo-3915915.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
+
+      <q-card-section>
+        <q-btn
+          fab
+          color="primary"
+          icon="place"
+          class="absolute"
+          style="top: 0; right: 12px; transform: translateY(-50%);"
+        />
+
+        <div class="row no-wrap items-center">
+          <div class="col text-h6 ellipsis">
+            Hamburguer X
+          </div>
+          <div class="col-auto text-grey text-caption q-pt-md row no-wrap items-center">
+            <q-icon name="place" />
+            250 ft
+          </div>
+        </div>
+
+        <q-rating v-model="stars" :max="5" size="32px" />
+      </q-card-section>
+
+      <q-card-section class="q-pt-none">
+        <div class="text-subtitle1">
+          $・Italian, Burguer
+        </div>
+        <div class="text-caption text-grey">
+          Small plates, salads & sandwiches in an intimate setting.
+        </div>
+      </q-card-section>
+
+      <q-separator />
+
+      <q-card-actions>
+        <q-btn flat round icon="event" />
+        <q-btn flat color="primary">
+          Reserve
+        </q-btn>
+      </q-card-actions>
+    </q-card>
+
+    <q-card class="my-card" flat bordered>
+      <q-img src="https://images.pexels.com/photos/845811/pexels-photo-845811.jpeg?auto=compress&cs=tinysrgb&w=600" />
+
+      <q-card-section>
+        <q-btn
+          fab
+          color="primary"
+          icon="place"
+          class="absolute"
+          style="top: 0; right: 12px; transform: translateY(-50%);"
+        />
+
+        <div class="row no-wrap items-center">
+          <div class="col text-h6 ellipsis">
+            Pizza
+          </div>
+          <div class="col-auto text-grey text-caption q-pt-md row no-wrap items-center">
+            <q-icon name="place" />
+            250 ft
+          </div>
+        </div>
+
+        <q-rating v-model="stars" :max="5" size="32px" />
+      </q-card-section>
+
+      <q-card-section class="q-pt-none">
+        <div class="text-subtitle1">
+          $・Italian, Cafe
+        </div>
+        <div class="text-caption text-grey">
+          Small plates, salads & sandwiches in an intimate setting.
+        </div>
+      </q-card-section>
+
+      <q-separator />
+
+      <q-card-actions>
+        <q-btn flat round icon="event" />
+        <q-btn flat color="primary">
+          Reserve
+        </q-btn>
+      </q-card-actions>
+    </q-card>
+
+    <q-card class="my-card" flat bordered>
+      <q-img src="https://images.pexels.com/photos/2983101/pexels-photo-2983101.jpeg?auto=compress&cs=tinysrgb&w=600" />
+
+      <q-card-section>
+        <q-btn
+          fab
+          color="primary"
+          icon="place"
+          class="absolute"
+          style="top: 0; right: 12px; transform: translateY(-50%);"
+        />
+
+        <div class="row no-wrap items-center">
+          <div class="col text-h6 ellipsis">
+            Hamburguer c/Fritas
+          </div>
+          <div class="col-auto text-grey text-caption q-pt-md row no-wrap items-center">
+            <q-icon name="place" />
+            250 ft
+          </div>
+        </div>
+
+        <q-rating v-model="stars" :max="5" size="32px" />
+      </q-card-section>
+
+      <q-card-section class="q-pt-none">
+        <div class="text-subtitle1">
+          $・Italian, Cafe
+        </div>
+        <div class="text-caption text-grey">
+          Small plates, salads & sandwiches in an intimate setting.
+        </div>
+      </q-card-section>
+
+      <q-separator />
+
+      <q-card-actions>
+        <q-btn flat round icon="event" />
+        <q-btn flat color="primary">
+          Reserve
+        </q-btn>
+      </q-card-actions>
+    </q-card>
+
+    <q-card class="my-card" flat bordered>
+      <q-img src="https://images.pexels.com/photos/3742373/pexels-photo-3742373.jpeg?auto=compress&cs=tinysrgb&w=600" />
+
+      <q-card-section>
+        <q-btn
+          fab
+          color="primary"
+          icon="place"
+          class="absolute"
+          style="top: 0; right: 12px; transform: translateY(-50%);"
+        />
+
+        <div class="row no-wrap items-center">
+          <div class="col text-h6 ellipsis">
+            Hamburguer Xl
+          </div>
+          <div class="col-auto text-grey text-caption q-pt-md row no-wrap items-center">
+            <q-icon name="place" />
+            250 ft
+          </div>
+        </div>
+
+        <q-rating v-model="stars" :max="5" size="32px" />
+      </q-card-section>
+
+      <q-card-section class="q-pt-none">
+        <div class="text-subtitle1">
+          $・Italian, Cafe
+        </div>
+        <div class="text-caption text-grey">
+          Small plates, salads & sandwiches in an intimate setting.
+        </div>
+      </q-card-section>
+
+      <q-separator />
+
+      <q-card-actions>
+        <q-btn flat round icon="event" />
+        <q-btn flat color="primary">
+          Reserve
+        </q-btn>
+      </q-card-actions>
+    </q-card>
+
+    <q-card class="my-card" flat bordered>
+      <q-img src="https://images.pexels.com/photos/3738762/pexels-photo-3738762.jpeg?auto=compress&cs=tinysrgb&w=600" />
+
+      <q-card-section>
+        <q-btn
+          fab
+          color="primary"
+          icon="place"
+          class="absolute"
+          style="top: 0; right: 12px; transform: translateY(-50%);"
+        />
+
+        <div class="row no-wrap items-center">
+          <div class="col text-h6 ellipsis">
+            Cheedar Burguer
+          </div>
+          <div class="col-auto text-grey text-caption q-pt-md row no-wrap items-center">
+            <q-icon name="place" />
+            250 ft
+          </div>
+        </div>
+
+        <q-rating v-model="stars" :max="5" size="32px" />
+      </q-card-section>
+
+      <q-card-section class="q-pt-none">
+        <div class="text-subtitle1">
+          $・Italian, Cafe
+        </div>
+        <div class="text-caption text-grey">
+          Small plates, salads & sandwiches in an intimate setting.
+        </div>
+      </q-card-section>
+
+      <q-separator />
+
+      <q-card-actions>
+        <q-btn flat round icon="event" />
+        <q-btn flat color="primary">
+          Reserve
+        </q-btn>
+      </q-card-actions>
+    </q-card>
+
+    <q-card class="my-card" flat bordered>
+      <q-img src="https://images.pexels.com/photos/2059151/pexels-photo-2059151.jpeg?auto=compress&cs=tinysrgb&w=600" />
+
+      <q-card-section>
+        <q-btn
+          fab
+          color="primary"
+          icon="place"
+          class="absolute"
+          style="top: 0; right: 12px; transform: translateY(-50%);"
+        />
+
+        <div class="row no-wrap items-center">
+          <div class="col text-h6 ellipsis">
+            Japan Ramen
+          </div>
+          <div class="col-auto text-grey text-caption q-pt-md row no-wrap items-center">
+            <q-icon name="place" />
+            250 ft
+          </div>
+        </div>
+
+        <q-rating v-model="stars" :max="5" size="32px" />
+      </q-card-section>
+
+      <q-card-section class="q-pt-none">
+        <div class="text-subtitle1">
+          $・Italian, Cafe
+        </div>
+        <div class="text-caption text-grey">
+          Small plates, salads & sandwiches in an intimate setting.
+        </div>
+      </q-card-section>
+
+      <q-separator />
+
+      <q-card-actions>
+        <q-btn flat round icon="event" />
+        <q-btn flat color="primary">
+          Reserve
+        </q-btn>
+      </q-card-actions>
+    </q-card>
+
+  </div>
+
 </q-page>
 </template>
 

18c3ef130fca76144b070a313c7beec0aec1cfd1
Gabriel Bitencourt <gb750779@gmail.com>
2023-12-26 10:22:24 -0300

    list error

--- src/pages/category/table.js
+++ src/pages/category/table.js
@@ -1,7 +1,7 @@
 const columnsCategory = [
   { name: 'name', align: 'left', label: 'Name', field: 'name', sortable: true },
   { name: 'id', align: 'left', label: 'Id', field: 'id', sortable: true },
-  { name: 'actions', align: 'right', label: 'Actions', field: 'actions', sortable: true }
+  { name: 'actions', align: 'right', label: 'Actions', field: 'actions', sortable: false }
 ]
 
 export {

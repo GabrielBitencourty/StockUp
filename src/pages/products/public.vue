ac6a3edace19b7b8f1b30d6facdd09dba8a64d45
Gabriel Bitencourt <gb750779@gmail.com>
2024-01-26 15:09:12 -0300

    Store Modes

--- src/pages/products/public.vue
+++ src/pages/products/public.vue
@@ -1,7 +1,108 @@
 <template>
-  <q-page padding>
-    <div class="row">
+  <div v-if="$q.platform.is.desktop">
+    <q-carousel
+      swipeable
+      animated
+      v-model="slide"
+      :autoplay="autoplay"
+      ref="carousel"
+      infinite
+      height="470px"
+      transition-prev="slide-right"
+      transition-next="slide-left"
+    >
 
+      <q-carousel-slide :name="1" img-src="../../../public/img/1.png"/>
+      <q-carousel-slide :name="2" img-src="../../../public/img/2.png"/>
+      <q-carousel-slide :name="3" img-src="../../../public/img/3.png"/>
+    </q-carousel>
+  </div>
+
+  <q-page padding>
+    <div class="payments" v-if="$q.platform.is.desktop">
+
+      <q-card padding class="flex flex-center justify-between">
+
+        <q-card-section>
+          <div class="flex flex-center">
+            <q-icon
+              name="mdi-contactless-payment-circle-outline"
+              size="md"
+              class="q-pr-md"
+              color="primary"
+            />
+            <div class="text-primary q-pt-md">
+              Pague no Pix
+              <p class="text-caption text-black">Com desconto de 10%</p>
+            </div>
+          </div>
+        </q-card-section>
+
+        <q-card-section>
+          <div class="flex flex-center">
+            <q-icon
+              name="mdi-credit-card-check-outline"
+              size="md"
+              class="q-pr-md"
+              color="primary"
+            />
+            <div class="text-primary q-pt-md">
+              Dividimos no Cartão
+              <p class="text-caption text-black">Em até 10x</p>
+            </div>
+          </div>
+        </q-card-section>
+
+        <q-card-section>
+          <div class="flex flex-center">
+            <q-icon
+              name="mdi-truck-fast"
+              size="md"
+              class="q-pr-md"
+              color="primary"
+            />
+            <div class="text-primary q-pt-md">
+              Entrega Rapida
+              <p class="text-caption text-black">Enviamos em até 5 dias</p>
+            </div>
+          </div>
+        </q-card-section>
+
+        <q-card-section>
+          <div class="flex flex-center">
+            <q-icon
+              name="mdi-account-convert"
+              size="md"
+              class="q-pr-md"
+              color="primary"
+            />
+            <div class="text-primary q-pt-md">
+              Devolução Gratis
+              <p class="text-caption text-black">Devolva sem custo em até 7 dias</p>
+            </div>
+          </div>
+        </q-card-section>
+
+        <q-card-section>
+          <div class="flex flex-center">
+            <q-icon
+              name="mdi-face-agent"
+              size="md"
+              class="q-pr-md"
+              color="primary"
+            />
+            <div class="text-primary q-pt-md">
+              Suporte 24h
+              <p class="text-caption text-black">Atendimento online pelo Whatsapp</p>
+            </div>
+          </div>
+        </q-card-section>
+
+      </q-card>
+
+    </div>
+
+    <div class="row q-pt-xl">
       <q-table
         grid
         class="my-sticky-virtscroll-table col-12"
@@ -15,7 +116,7 @@
 
         <template v-slot:top>
           <span class="text-h6">
-            Products
+            Catalogo de Produtos
           </span>
           <q-space />
           <q-input
@@ -72,6 +173,7 @@
                 class="bg-primary q-mb-md text-white"
                 rounded
                 no-caps
+                disable
                 icon="mdi-shopping"
               >
               </q-btn>
@@ -147,7 +249,10 @@ export default defineComponent({
       filter,
       productDetails,
       showDialogDetails,
-      handleShowDetails
+      handleShowDetails,
+
+      slide: ref(1),
+      autoplay: ref(true)
     }
   }
 })
